ada1750a6c5e7084e3812b6eb733f74d26d9cda9
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2021-02-28 15:51:13 -0500

    Added window instruction image to readme file.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace PigeonsTP2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap window_instruction {
+            get {
+                object obj = ResourceManager.GetObject("window_instruction", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
